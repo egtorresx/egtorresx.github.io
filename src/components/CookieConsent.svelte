41374ab0d4cb96b09ae6ebb63f4bc62ef434c984
Edison Torres <egtorresx@utpl.edu.ec>
2022-02-23 12:46:13 -0500

    make cookie consent buttons smaller on sm breakpoint

--- src/components/CookieConsent.svelte
+++ src/components/CookieConsent.svelte
@@ -14,18 +14,25 @@
 
 {#if visible}
   <div
-    class="bg-blue-300 text-blue-900 fixed bottom-0 right-0 w-full px-10 py-2 flex justify-between items-center"
+    class="bg-blue-300 text-blue-900 fixed bottom-0 
+    right-0 w-full md:px-10 md:py-3 px-2 py-1 flex justify-between items-center"
   >
-    <span>
-      Este sitio web utiliza cookies para asegurarse que tenga la mejor
-      experiencia.
-      <a class="font-bold" href="https://www.cookiesandyou.com/" target="_blank"
-        >Aprende más</a
+    <span class="text-sm md:text-lg flex-1">
+      ¿Acepta el uso cookies para asegurarse que tenga la mejor experiencia en
+      este sitio web?
+      <a
+        class="font-bold text-sm md:text-lg"
+        href="https://www.cookiesandyou.com/"
+        target="_blank">Aprende más</a
       >
     </span>
     <div>
-      <button class="btn btn-error" on:click={deniedCookies}>No Acepto</button>
-      <button class="btn btn-success" on:click={acceptCookies}>Acepto</button>
+      <button class="btn btn-error btn-sm md:btn-md" on:click={deniedCookies}
+        >No</button
+      >
+      <button class="btn btn-success btn-sm md:btn-md" on:click={acceptCookies}
+        >Si</button
+      >
     </div>
   </div>
 {/if}
