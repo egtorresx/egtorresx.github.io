12e7f1f4d81aa2cf160c1fad7c9dc2edf4c12c3c
Edison Torres <egtorresx@gmail.com>
2022-08-22 17:25:37 -0500

    update svelte-kit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import CookieConsent from "../components/CookieConsent.svelte";
+  import GaTag from "../components/GaTag.svelte";
+  import "../app.css";
+</script>
+
+<main class="container mx-auto px-10 pt-2 md:pt-10">
+  <slot />
+  <CookieConsent />
+  <GaTag />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Hero from "../components/Hero.svelte";
+  import Skills from "../components/Skills.svelte";
+  import Achievements from "../components/Achievements.svelte";
+  import Timeline from "../components/Timeline.svelte";
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<Hero />
+<Skills />
+<Achievements />
+<Timeline />
+<Footer />
+
+<svelte:head>
+  <title>Edison Torres | Desarrollador de Software</title>
+</svelte:head>
