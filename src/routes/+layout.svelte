2e86ecad113ea2bd63ebc4d6a338826104869985
Edison Torres <egtorresx@gmail.com>
2023-08-12 10:08:10 -0500

    resolver error on build para favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,12 @@
   import CookieConsent from "../components/CookieConsent.svelte";
   import GaTag from "../components/GaTag.svelte";
   import "../app.css";
+  import { base } from "$app/paths";
 </script>
+
+<svelte:head>
+  <link rel="icon" href="{base}/favicon.svg" />
+</svelte:head>
 
 <main class="container mx-auto px-10 pt-2 md:pt-10">
   <slot />
--- svelte.config.js
+++ svelte.config.js
@@ -13,9 +13,10 @@ const config = {
     // hydrate the <div id="svelte"> element in src/app.html
     // target: "#svelte",
     adapter: adapter(),
-    // paths: {
-    //   base: dev ? "" : "/egtorresx",
-    // },
+    paths: {
+      base:
+        process.env.NODE_ENV === "production" ? "/sveltekit-github-pages" : "",
+    },
     // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
     // For example, instead of '_app', use 'app_', 'internal', etc.
     appDir: "internal",
